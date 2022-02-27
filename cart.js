4108ebe1435778745281a5a788a71c25118672e1
NobleSincoder <sincode.reeves@gmail.com>
2022-02-26 19:35:33 -0700

    fixed alot

--- cart.js
+++ cart.js
@@ -1,40 +1,73 @@
-let cart = []
-let shirt = 'shirt'
-let underthere = 'underwear'
+
+
+let item = {
+    shirt: "Graphic T-Shirt",
+    shoes: "Air Jordans",
+    socks: "Stance Socks",
+    hat: "Hat depicting your favorite game/tv show",
+    underthere: "underwear"
+}
 
 
 
-function myCart(key){
-
+function myCart(key, item, cart){
+    
     switch(key){
         case "clear": 
-        // Clear Cart
-            newCart = []
-            console.log(newCart)
-            break;
-        case "remove 1": 
-        // Remove single Item
-            console.log(cart.pop())
-            cart.pop()
-            newCart = cart
-            console.log(newCart)
-            break;
+            // Clear Cart
+            newCart = []            
+            return newCart;
+        
+        case "remove_1": 
+        // Remove single Item        
+            newCart = [...cart]
+            newCart.splice(newCart.indexOf(item),1)     
+            return newCart;     
+
         case "add": 
         // Add Items
-            newCart = cart
-            cart.push(shirt);
-            cart.push(underthere);
-            newCart = cart;
-            console.log(newCart);
-            break;
-        case "remove all": 
+            newCart = [...cart, item] 
+            return newCart;
+        
+        case "remove_all": 
         // Remove All Items
             newCart = []
-            console.log(newCart)
-            break;
-
-    }
+            for (let i of cart){
+                
+                if (i != item){
+                    newCart.push(i)
+                    console.log(newCart)
+                };
+            };
+            return newCart;
+        
+        
+    };
+    
 };
 
+let leCart = []
 
-myCart("add")
+let aCart = myCart("add", item.socks, leCart)
+console.log(aCart)
+aCart = myCart("add", item.socks, aCart)
+console.log(aCart)
+aCart = myCart("add", item.shirt, aCart)
+console.log(aCart)
+aCart = myCart("add", item.hat, aCart)
+console.log(aCart)
+aCart = myCart("remove_all", item.socks, aCart)
+console.log(aCart)
+aCart = myCart("add", item.underthere, aCart)
+console.log(aCart)
+aCart = myCart("add", item.underthere, aCart)
+console.log(aCart)
+aCart = myCart("add", item.shirt, aCart)
+console.log(aCart)
+aCart = myCart("remove_1", item.underthere, aCart)
+console.log(aCart)
+aCart = myCart("add", item.shoes, aCart)
+console.log(aCart)
+aCart = myCart("clear", item.shoes, aCart)
+console.log(aCart)
+
